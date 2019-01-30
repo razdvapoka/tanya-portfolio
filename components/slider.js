dddd60bc23eab5e06195d181007f39ef3a0429a0
razdvapoka <razdvapoka@gmail.com>
2019-01-30 23:51:23 +0400

    :sparkles: Animate sliders

--- components/slider.js
+++ components/slider.js
@@ -5,10 +5,20 @@ import {
 } from 'pss-components'
 import { ps } from 'pss'
 import React from 'react'
-
 import { pxToRem } from '../constants'
+import anime from 'animejs'
+import styled from '@emotion/styled'
+
+const VELOCITY = 0.05
+const InlineFlexBox = styled(FlexBox)({
+  display: 'inline-flex'
+})
 
 class Slider extends React.Component {
+  state = {
+    animationTimeline: null
+  }
+
   render () {
     const { items } = this.props
     return (
@@ -16,11 +26,16 @@ class Slider extends React.Component {
         position='absolute' left top width height
         alignItems='center'
         justifyContent='center'
+        onClick={this.toggleAnimation}
       >
-        <Box ov='auto' pdy={10}>
-          <FlexBox
+        <Box
+          ov='hidden'
+          pdy={10}
+        >
+          <InlineFlexBox
             alignItems='center'
             flexWrap={false}
+            ref={ref => { this.slides = ref }}
           >
             {items.map(item => {
               const image = item.fields.image.fields.file
@@ -37,10 +52,43 @@ class Slider extends React.Component {
                 </Box>
               )
             })}
-          </FlexBox>
+          </InlineFlexBox>
         </Box>
       </FlexBox>
     )
+  }
+
+  animateSlides = () => {
+    const rect = this.slides.getBoundingClientRect()
+    const distance = rect.width - window.innerWidth
+    const timeline = anime.timeline({
+      duration: distance / VELOCITY,
+      easing: 'linear',
+      autoplay: true,
+      direction: 'alternate',
+      loop: true
+    })
+    timeline.add({
+      targets: this.slides,
+      translateX: -distance
+    })
+    this.setState({
+      animationTimeline: timeline
+    })
+  }
+
+  toggleAnimation = () => {
+    const { animationTimeline } = this.state
+    if (animationTimeline.paused) {
+      animationTimeline.reverse()
+      animationTimeline.play()
+    } else {
+      animationTimeline.pause()
+    }
+  }
+
+  componentDidMount () {
+    this.animateSlides()
   }
 }
 
